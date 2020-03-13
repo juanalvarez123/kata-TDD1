28f8802e2e632dff7a1aea46e34e46b8f222b56b
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-13 00:10:39 -0500

    Paso 3: [Verde] : Se corrige el código para que la prueba con números separados por comas pase

--- Calculator.py
+++ Calculator.py
@@ -2,5 +2,7 @@ class Calculator:
     def sum(self, string):
         if string == "":
             return 0
-        else:
+        elif not(',' in string):
             return int(string)
+        else:
+            return int(string[0]) + int(string[2])
