0ac15c85126673c6ec35eaf33053bab891c2c9ab
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-12 23:44:30 -0500

    Paso 2: [Rojo] : Se agrega prueba para probar con un número

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -6,3 +6,6 @@ from Calculator import Calculator
 class CalculatorTest(TestCase):
     def test_sum(self):
         self.assertEqual(Calculator().sum(""), 0, "Empty string")
+
+    def test_sum_string(self):
+        self.assertEqual(Calculator().sum("1"), 1, "A number")
